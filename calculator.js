a495106b81cbeb9bc25d2ed76f7f1f19ad25cace
Muhydeen Mustapha <muhydeen.mustapha100@gmail>
2024-08-02 15:16:58 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,28 @@
+
+
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function formatNumber(number) {
+    return new Intl.NumberFormat('en-US').format(number);
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        const result = eval(display.innerText);
+        display.innerText = formatNumber(result);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
